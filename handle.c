3d4767603c5b65cb4d34836deb1cf6c89f3b4320
Haoyu Gong <hcgong@cs.utexas.edu>
2016-02-01 18:26:28 -0600

    finished 1.2

--- handle.c
+++ handle.c
@@ -7,6 +7,18 @@
 #include <unistd.h>
 #include "util.h"
 
+/*
+ * Handler for the SIGINT signal
+ * prints nice try and then continues looping
+ */
+void sigint_handler(int sig) 
+{
+    ssize_t bytes; 
+    const int STDOUT = 1; 
+    bytes = write(STDOUT, "Nice Try.\n", 10); 
+    if(bytes != 10) 
+       exit(-999);
+}
 
 /*
  * First, print out the process ID of this process.
@@ -18,8 +30,20 @@
  * second.
  */
 int main(int argc, char **argv)
-{
-  return 0;
+{	
+	struct timespec req, remain, remain2;
+	pid_t pid = getpid();
+
+	printf("%d\n", pid);
+	req.tv_sec = 1;
+	signal(SIGINT, sigint_handler);
+
+	while(1) {
+		printf("Still here\n");
+		if(nanosleep(&req, &remain) < 0) {
+			nanosleep(&remain, &remain2);
+		}
+	}
+
+  	return 0;
 }
-
-
